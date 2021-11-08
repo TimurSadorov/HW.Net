c30c8d61dfab5754957f0b145a4386b4eb445a99
Sadorov Timur <sadorov2001@mail.ru>
2021-11-08 20:29:13 +0300

    HW8

---
+++ src/Homeworks/Homework8/Logic/NumericalCalculator.cs
@@ -0,0 +1,19 @@
+﻿using Homework8.Interface;
+
+namespace Homework8
+{
+	public class NumericalCalculator : ICalculator
+	{
+		public double Add(double firstValue, double secondValue)
+			=> firstValue + secondValue;
+
+		public double Subtract(double firstValue, double secondValue)
+			=> firstValue - secondValue;
+
+		public double Divide(double firstValue, double secondValue)
+			=> secondValue == 0 ? 0 : firstValue / secondValue;
+
+		public double Multiply(double firstValue, double secondValue)
+			=> firstValue * secondValue;
+	}
+}
